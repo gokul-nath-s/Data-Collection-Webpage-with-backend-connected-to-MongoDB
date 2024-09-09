bc55030a99387a0ca56eaf04fdd8de54d59e27ee
Gokul Nath S <12345.gokulnath.s@gmail.com>
2024-09-09 21:06:51 +0530

    Initial Commit

---
+++ Frontend/src/Pages/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import myImage from 'D:\Programming\React\ReactProject\Frontend\src\Pages\assimage.jpg';
+
+const Hero=()=>{
+    return(
+        <div>
+            <img src={myImage} alt="hello"/>
+            </div>
+    );
+};
+export default Hero;
